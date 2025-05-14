7ca07e98210c5a0e3b50cede905ab3541d16fda8
Miguel Suarez <miasuarezco@unal.edu.co>
2025-05-13 21:59:46 -0500

    Implemented new arch, init works now

---
+++ data/postgres/users/init-postgres.sql
@@ -0,0 +1,16 @@
+-- Step 1: Create the database
+CREATE DATABASE devpu;
+
+-- Step 2: Connect to the devp database
+\c devpu;
+
+-- Step 3: Create tables
+
+-- User table
+CREATE TABLE Users (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    name VARCHAR(100) NOT NULL
+);
+
