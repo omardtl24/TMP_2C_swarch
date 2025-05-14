a9e486d059e00be0c956364ad42857919ddd287b
juandp1 <jupalaciosf@unal.edu.co>
2025-05-14 18:26:20 -0500

    navbar initial config

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -1,10 +1,24 @@
-const navbar = () => {
+import { Menu } from "lucide-react";
+import Image from "next/image";
+import { UserRound } from 'lucide-react';
+
+const Navbar = () => {
     return(
 
-        <nav className="bg-background h-16 w-full flex flex-row items-center md:items-start justify-between px-4">
-            
+        <nav className="sticky top-0 right-0 left-0 bg-surface h-16 w-full flex flex-row items-center  justify-between px-4 rouned-b-md">
+            <Menu className="md:hidden" />
+            <Image
+                src="/images/Cuentas_claras_icono.png"
+                alt="Logo"
+                width={50}
+                height={50}
+                className="md:block"
+            />
+            <UserRound className=" md:block" />
+                
+
         </nav>
     )
 }
 
-export default navbar;
+export default Navbar;
