d002df00edfba9d3daf429117fe62e4e5ef4c86d
mcsanchezp <mc.sanchezp1@uniandes.edu.co>
2025-05-13 21:21:41 -0500

    add services and controllers for logic-personal

--- logic-personal/src/main/java/com/cuentas_claras/backend/BackendApplication.java
+++ logic-personal/src/main/java/com/cuentas_claras/backend/BackendApplication.java
@@ -1,9 +1,13 @@
 package com.cuentas_claras.backend;
 
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.cuentas_claras.backend.repositories.sql")
@@ -13,5 +17,10 @@ public class BackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
+    }
+
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
