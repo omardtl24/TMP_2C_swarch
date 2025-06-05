b15c6d1faf06066476c8f64faa2b3e50bfd2c6b8
GianK19 <glanziano@unal.edu.co>
2025-06-04 20:54:46 -0500

    add cors in api

--- lg-apigateway-oc/main.py
+++ lg-apigateway-oc/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from gateway.routers.auth_router import router as auth_router
 from gateway.routers.events_router import router as events_router
 from gateway.routers.expenses_router import router as expenses_router
 from gateway.routers.personal_router import router as personal_router
 
 app = FastAPI(title="API Gateway")
+
+origins = [  
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
